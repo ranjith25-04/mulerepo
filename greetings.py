556d18e4d20ac1731736ec6ffcb4e6811e9ca772
ranjith <ranjith.reddy284@gmail.com>
2020-07-30 14:55:38 +0530

    commited after knowning git diff

--- greetings.py
+++ greetings.py
@@ -1,2 +1,4 @@
 print("welcome!")
 print("How are you!")
+
+#lets check with two files
--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,6 @@
 print("Hello world!")
 print("Where are you!")
+
+# i am trying git diff
+
+#lets see the out put
